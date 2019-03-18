28674c9ac14a70d7a34a63b4bd7a8607d9fac413
ayoubM2i <ayo.daif@gmail.com>
2019-03-18 10:47:01 +0100

    commit 10h42 (voir  cahier ) debut application angular pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { NewsComponent } from './news/news.component';
+import { ConvComponent } from './conv/conv.component';
+
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    ConvComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/conv/conv.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conv',
+  templateUrl: './conv.component.html',
+  styleUrls: ['./conv.component.css']
+})
+export class ConvComponent implements OnInit {
+
+  private montantEuro : number;
+  private montantFranc : number;
+
+  public onConversion(event:any){
+    this.montantFranc =6.5597 * Number(this.montantEuro);  // obligé de rajouter number ici sinon il va pas savoir que c'est un number...psk le typscript va etre converte en js et le number va disparaitre donc ça peut nous poser de sprb....// remarque dans angular pas besoin d aller chercher dans la zone la valeur..c'est le html qui va le faire
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
